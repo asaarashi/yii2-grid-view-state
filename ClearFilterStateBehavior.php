d89cb2f94fc8adab800e8445aa0278a0291f6671
shiberiu <shiberiu@gmail.com>
2016-01-22 17:22:33 +0800

    Enhance ClearFilterStateBehavior

--- ClearFilterStateBehavior.php
+++ ClearFilterStateBehavior.php
@@ -10,6 +10,7 @@ class ClearFilterStateBehavior extends ActionFilter {
     public $id;
     public $params;
     public $clearStateParam = 'clear-state';
+    public $exitIfAjax = true;
 
     public function beforeAction($action) {
         if(($params = $this->params) === null) {
@@ -19,6 +20,14 @@ class ClearFilterStateBehavior extends ActionFilter {
 
         if(isset($params[$this->clearStateParam]) && $params[$this->clearStateParam] === '1') {
             FilterStateTrait::clearFilterStateParams($this->id);
+
+            if(Yii::$app->request->isAjax && $this->exitIfAjax) {
+                Yii::$app->end();
+            } else {
+                $params = Yii::$app->request->getQueryParams();
+                unset($params[$this->clearStateParam]);
+                Yii::$app->request->setQueryParams($params);
+            }
         }
     }
 }
